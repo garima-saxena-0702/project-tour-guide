3b80ac80179ba5eb40d16f3b519569a3bc7df079
Garima Saxena <GarimaSaxena0702@gmail.com>
2018-12-01 09:00:57 +0530

    expressadding dist

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, '/dist/project')));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src/index.html'));
+  console.log(path.join(__dirname, 'dist/project/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/project/index.html'));
 });
 
 app.listen( 4200, function(){
-    console.log('Your node js server is running');
+    console.log('Server is running');
 });
